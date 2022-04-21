567f27255e8823dad08a3c1d25233ac53fbbdf8f
BrianPinaud <brianepinaud@gmail.com>
2022-04-21 20:38:24 -0300

    creacion de las entidades fotografo y cliente

---
+++ src/main/java/com/ProyectoFinal/FotoJob/entidades/Cliente.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ProyectoFinal.FotoJob.entidades;
+
+/**
+ *
+ * @author brian
+ */
+public class Cliente {
+    
+}
