e174d31e601f71f6d2caee86c2cb081ec2beeabc
GPR <rochafrancoagustin@hotmail.com>
2022-04-20 21:45:13 -0300

    Se agrego y configuro el proyecot base de Spring

---
+++ src/main/java/com/ProyectoFinal/FotoJob/FotoJobApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoFinal.FotoJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotoJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotoJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProyectoFinal/FotoJob/FotoJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoFinal.FotoJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FotoJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
